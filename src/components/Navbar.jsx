0f5aa8a48d6659dd9e484febd964ddf39d510b02
D <lorrainedams@me.com>
2020-01-13 16:22:25 +0100

    changement couleur

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,43 @@
 import React, { useState } from "react";
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler,
+  Collapse
+} from "reactstrap";
 
 function NavbarTop() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
   return (
     <>
       <Navbar light expand="md">
         <NavbarBrand href="/">
           <img src="images/minilogopoza.png" alt="logo poza" />
         </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="https://poza-evenement.fr/">L'agence POZA</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://poza-evenement.fr/contact/">
+                Contact
+              </NavLink>
+            </NavItem>
 
-        <Nav className="mr-auto" navbar>
-          <NavItem>
-            <NavLink href="https://poza-evenement.fr/">L'agence POZA</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://poza-evenement.fr/contact/">Contact</NavLink>
-          </NavItem>
-
-          <NavItem>
-            <NavLink href="http://localhost:8001/new">
-              Devenez prestataire
-            </NavLink>
-          </NavItem>
-        </Nav>
+            <NavItem>
+              <NavLink href="http://localhost:8001/new">
+                Devenez prestataire
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
     </>
   );
